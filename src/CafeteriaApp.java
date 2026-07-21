6fa079d9e9617967cf9808f75268c3b85b11a5c4
Marvin Rios <mriost1@upao.edu.pe>
2026-07-21 13:31:02 -0500

    feat(main): integrar ejecucion del patron Factory Method en clase principal

--- src/CafeteriaApp.java
+++ src/CafeteriaApp.java
@@ -1,2 +1,21 @@
 public class CafeteriaApp {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("   BIENVENIDOS A COFFEE CODE SHOP   ");
+        System.out.println("=========================================");
+
+        FactoryPay fabrica;
+
+        // 1. Cliente paga en Caja con Tarjeta POS
+        fabrica = new FactoryPayPOS(18.50, "4557880012349981");
+        fabrica.ejecutarTransaccion();
+
+        // 2. Cliente paga por la App Móvil con Yape (QR)
+        fabrica = new FactoryPayQR(24.00, "987654321", "YAPE");
+        fabrica.ejecutarTransaccion();
+
+        // 3. Cliente hace pedido web y paga Online
+        fabrica = new FactoryPayWeb(45.00, "juan.perez@email.com", "TOK_SECURE_9921");
+        fabrica.ejecutarTransaccion();
+    }
 }
